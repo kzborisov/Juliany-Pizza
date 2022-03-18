135ff9be848090077a33446e8d78eefc5c02787b
kborisov <kborisov@lely.com>
2022-03-18 12:53:59 +0200

    Refactor the menu and cart in relation to the new menu database

--- juliany_pizza/cart/cart.py
+++ juliany_pizza/cart/cart.py
@@ -1,3 +1,6 @@
+from juliany_pizza.menu.models import Product
+
+
 class Cart:
     SESSION_KEY = 'session-key'
 
@@ -8,19 +11,38 @@ class Cart:
             cart = self.session[self.SESSION_KEY] = {}
         self.cart = cart
 
-    def add(self, item, size):
-        size_id = str(size.id)
-        if size_id in self.cart:
-            self.cart[size_id]['qty'] += 1
-        else:
-            self.cart[size_id] = {
-                'name': item.name,
-                'price': float(size.price),
-                'size': size.size,
+    def add(self, item):
+        """
+        Add items to the cart.
+        :param item: Menu Item object
+        :param size: Size object
+        """
+        item_id = str(item.id)
+        if item_id not in self.cart:
+            self.cart[item_id] = {
+                'productId': item.product.id,
+                'price': float(item.price),
+                'size': item.size if not item.size == '---' else None,
                 'qty': 1,
             }
+        else:
+            self.cart[item_id]['qty'] += 1
 
         self.session.modified = True
 
+    def __iter__(self):
+        """
+        Collect the cart keys to query the database and return products
+        :return:
+        """
+        size_ids = self.cart.keys()
+        menu_items = Product.objects.filter(size__id__in=size_ids)
+        cart = self.cart.copy()
+        yield menu_items
+
     def __len__(self):
+        """
+        Calculates the items quantity.
+        :return: The quantity of all items in the basket.
+        """
         return sum(item['qty'] for item in self.cart.values())
--- juliany_pizza/cart/views.py
+++ juliany_pizza/cart/views.py
@@ -4,29 +4,16 @@ from django.views import View
 from django.views.generic import TemplateView
 
 from juliany_pizza.cart.cart import Cart
-from juliany_pizza.menu.models import Size, MenuItem
+from juliany_pizza.menu.models import Stock
 
 
 class CartSummaryView(TemplateView):
     template_name = 'cart/summary.html'
 
-
-#
-# def add_to_cart(request):
-#     cart = Cart(request)
-#     if request.POST.get('action') == 'post':
-#         item_id = int(request.POST.get('itemId'))
-#         item_size = request.POST.get('itemSize')
-#         item = get_object_or_404(MenuItem, id=item_id)
-#         size = get_object_or_404(Size, size=item_size, menuitem__id=item_id)
-#         cart.add(item=item, size=size)
-#         response = JsonResponse(
-#             {
-#                 # TODO: Replace with actual data
-#                 'test': 'data',
-#             }
-#         )
-#         return response
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart'] = Cart(self.request)
+        return context
 
 
 class AddToCartView(View):
@@ -34,10 +21,8 @@ class AddToCartView(View):
         cart = Cart(request)
         if request.POST.get('action') == 'post':
             item_id = int(request.POST.get('itemId'))
-            item_size = request.POST.get('itemSize')
-            item = get_object_or_404(MenuItem, id=item_id)
-            size = get_object_or_404(Size, size=item_size, menuitem__id=item_id)
-            cart.add(item=item, size=size)
+            item = get_object_or_404(Stock, id=item_id)
+            cart.add(item=item)
             response = JsonResponse(
                 {
                     'data': cart.cart,
