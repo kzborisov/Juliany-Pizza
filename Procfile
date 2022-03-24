9ff8249f7f437f090bd3d6dbd178be82a7a53683
kborisov <kborisov@lely.com>
2022-03-24 12:36:52 +0200

    Added Procfile and gunicorn to requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn juliany_pizza.wsgi
--- juliany_pizza/settings.py
+++ juliany_pizza/settings.py
@@ -161,5 +161,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+# TODO: Do not forget to change it at the end!!!
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
