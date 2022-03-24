faddcb4d35ac9111386a6cc68782bfc925e8b0a6
kborisov <kborisov@lely.com>
2022-03-24 16:42:44 +0200

    Set DEBUG to False

--- juliany_pizza/settings.py
+++ juliany_pizza/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+my4xblgta@co3#hscc7j2pw+ir*biwtqv3v&s2+a&#9509cr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -89,13 +89,6 @@ WSGI_APPLICATION = 'juliany_pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -104,9 +97,8 @@ DATABASES = {
         'PASSWORD': 'abcadc01f59c52f13f5634f95dcc155d04feeffef650df181e2b22387a2b596f',
         'HOST': 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
-    }
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
